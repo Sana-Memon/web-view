cdc4dd1e473e1af44bd0621785cc4439ca77f467
Sanaa-hub <sanaamemon567@gmail.com>
2023-04-26 00:03:24 +0500

    working on webview

---
+++ lib/mainscreen.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewContainer extends StatefulWidget {
+  final url;
+  WebViewContainer(this.url);
+  @override
+  createState() => _WebViewContainerState(this.url);
+}
+
+class _WebViewContainerState extends State<WebViewContainer> {
+  var _url;
+  final _key = UniqueKey();
+  _WebViewContainerState(this._url);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Expanded(
+                child: WebView(
+                    key: _key,
+                    javascriptMode: JavascriptMode.unrestricted,
+                    initialUrl: _url))
+          ],
+        ));
+  }
+}
